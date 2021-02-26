c07c769b131ddd7e3018ef362e180d02212a2314
kedeinroga <keybroga@gmail.com>
2021-02-26 00:58:03 -0500

    styles added to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,25 @@
-import React, { Fragment } from "react";
+import React from "react";
 import logo from "./../assets/images/crashcourse.svg";
 import "./header.css";
 
 function Header() {
   return (
-    <Fragment>
-      <img src={logo} alt="Crashcourse" />
-      <input type="checkbox" id="hamburger-check"></input>
-      <label for="hamburger-check" class="hamburger">
-        <span></span>
-        <span></span>
-        <span></span>
-      </label>
-    </Fragment>
+    <div className="nav-mobile">
+      <div className="navtoggle">
+        <input type="checkbox" id="hamburger-check"></input>
+        <label htmlFor="hamburger-check" className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+        <div>
+          <i className="ri-earth-fill"></i>
+          <img src={logo} alt="Crashcourse" />
+        </div>
+        <i className="ri-search-line"></i>
+        {/* here content if there is something to show after clicking on the burger */}
+      </div>
+    </div>
   );
 }
 
