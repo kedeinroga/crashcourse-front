cb30d387eaffded2a25331187154c6e1cd17e2fa
kedeinroga <keybroga@gmail.com>
2021-02-26 02:25:38 -0500

    categories in process, the page is still missing

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 
 import Header from "./components/header";
 import Hero from "./components/hero";
+import Categories from "./components/categories";
 
 function App() {
   return (
     <Fragment>
       <Header />
       <Hero />
+      <Categories />
     </Fragment>
   );
 }
