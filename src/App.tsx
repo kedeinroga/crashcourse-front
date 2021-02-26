216f766436c42a7b4070cdff7107aa769b67a193
kedeinroga <keybroga@gmail.com>
2021-02-26 01:34:21 -0500

    hero styles are working

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from "./assets/images/crashcourse.svg";
 import "./App.css";
 
 import Header from "./components/header";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Hero />
     </Fragment>
   );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero__title">
+        <h2>Title H2 - banner cursos nuevos</h2>
+      </div>
+      <button className="button__comprar">Comprar ahora</button>
+    </div>
+  );
+}
+
+export default Hero;
