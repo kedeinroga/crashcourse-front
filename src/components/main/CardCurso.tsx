2b9e7da76b0042ab3aa98e44dc6ca9b36974cfa9
kedeinroga <keybroga@gmail.com>
2021-02-26 22:00:51 -0500

    course list and course card work styles

--- src/components/main/CardCurso.tsx
+++ src/components/main/CardCurso.tsx
@@ -4,8 +4,32 @@ import Button from "../UI/Buttons/Button";
 function CardCurso() {
   return (
     <div className="card-curso">
-      <p>Curso</p>
-      <Button />
+      <div className="curso-banner">
+        <div className="curso-tag">TAG VENTA</div>
+      </div>
+      <div className="curso-description">
+        <div className="curso-description__name">Nombre del curso </div>
+        <p className="curso-description__prof">Profesor del curso</p>
+        <div className="curso-description__info">
+          <div className="curso-description__info--data">
+            <i className="ri-bar-chart-line"></i>
+            <p>Nivel 1</p>
+          </div>
+          <div className="curso-description__info--data">
+            <i className="ri-user-fill"></i>
+            <p>123 Usuarios</p>
+          </div>
+          <div className="curso-description__info--data">
+            <i className="ri-star-fill"></i>
+            <p>4.8</p>
+          </div>
+        </div>
+        <div className="curso-cost">
+          <p className="ofert-price">CO$164,434</p>
+          <p className="real-price">co$349,929</p>
+        </div>
+        <Button />
+      </div>
     </div>
   );
 }
--- src/components/main/ListCurso.tsx
+++ src/components/main/ListCurso.tsx
@@ -7,8 +7,9 @@ import ButonFilter from "../UI/Buttons/ButonFilter";
 function ListCurso() {
   return (
     <div className="list-curso">
-      <h4>Title H4 - Listado de cursos</h4>
+      <h4 className="list-curso__title">Title H4 - Listado de cursos</h4>
       <ButonFilter />
+      <CardCurso />
       <CardCurso />
     </div>
   );
