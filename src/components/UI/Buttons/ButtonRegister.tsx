3623c9c0812e7d2e905d8b6ed7fa6dcdfcd176c4
kedeinroga <keybroga@gmail.com>
2021-02-26 22:35:16 -0500

    header in version desktop

---
+++ src/components/UI/Buttons/ButtonRegister.tsx
@@ -0,0 +1,7 @@
+import "./ButtonRegister.css";
+
+function ButtonRegister() {
+  return <button className="button__register"> Registrate gratis</button>;
+}
+
+export default ButtonRegister;
--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
 import logo from "../../assets/images/crashcourse.svg";
 import "./header.css";
+import ButtonRegister from "../UI/Buttons/ButtonRegister";
 
 function Header() {
   return (
-    <div className="nav-mobile">
-      <div className="navtoggle">
-        <input type="checkbox" id="hamburger-check"></input>
-        <label htmlFor="hamburger-check" className="hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
+    <Fragment>
+      <div className="nav-mobile">
+        <div className="navtoggle">
+          <input type="checkbox" id="hamburger-check"></input>
+          <label htmlFor="hamburger-check" className="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+          <div>
+            <i className="ri-earth-fill"></i>
+            <img src={logo} alt="Crashcourse" />
+          </div>
+          <i className="ri-search-line"></i>
+          {/* here content if there is something to show after clicking on the burger */}
+        </div>
+      </div>
+      <div className="nav-desktop">
         <div>
           <i className="ri-earth-fill"></i>
           <img src={logo} alt="Crashcourse" />
         </div>
-        <i className="ri-search-line"></i>
-        {/* here content if there is something to show after clicking on the burger */}
+        <ButtonRegister />
       </div>
-    </div>
+    </Fragment>
   );
 }
 
