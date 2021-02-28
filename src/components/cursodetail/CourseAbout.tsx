155c4cbccf477b9521aa2689911262921c6a5e8a
kedeinroga <keybroga@gmail.com>
2021-02-27 21:31:52 -0500

    curse about component styles are loaded

--- src/components/cursodetail/CourseAbout.tsx
+++ src/components/cursodetail/CourseAbout.tsx
@@ -2,9 +2,9 @@ import "./CourseAbout.css";
 
 function CourseAbout() {
   return (
-    <div>
+    <div className="course-about__content">
       <h3>H3 - ¿De qué trata este curso?</h3>
-      <p>
+      <p className="course-about__content--paragraph">
         Body 3- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id mi,
         mauris aliquam phasellus quis semper diam fringilla. Nunc nullam est in
         non. Enim sapien amet ut pharetra. Purus sagittis est felis sapien in.
@@ -14,6 +14,10 @@ function CourseAbout() {
         Purus sagittis est felis sapien in. Auctor tellus, in mauris mi,
         facilisi arcu sedut.
       </p>
+      <select className="select-about" name="link" id="link">
+        <option value="textlink">textlink</option>
+        <option value="textlink1">textlink1</option>
+      </select>
     </div>
   );
 }
