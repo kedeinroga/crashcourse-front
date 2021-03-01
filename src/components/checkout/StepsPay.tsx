ccf588177aefffb72fa2f38b8b6cb0b675c56dcf
kedeinroga <keybroga@gmail.com>
2021-02-28 19:49:02 -0500

    component styles steps to pay in mobile version

--- src/components/checkout/StepsPay.tsx
+++ src/components/checkout/StepsPay.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./StepsPay.css";
 
 function StepsPay() {
-  return <div>StepsPay</div>;
+  return (
+    <div className="steps-pay">
+      <div className="step">
+        <div className="step-circle one">1</div>
+        <p className="step-one">Proceso de pago</p>
+      </div>
+      <i className="ri-arrow-right-s-line"></i>
+      <div className="step">
+        <div className="step-circle two">2</div>
+        <p className="step-two">Paso 2</p>
+      </div>
+    </div>
+  );
 }
 
 export default StepsPay;
